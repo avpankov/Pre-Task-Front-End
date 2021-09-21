31bd9bbc5376221a6d390166f22c32a54c2fa769
avpankov <77238369+avpankov@users.noreply.github.com>
2021-09-21 10:52:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const commentArea = document.getElementById('comment'),
     
 let regexp = /^(\+375)(29|25|44|33)(\d{3})(\d{2})(\d{2})$/;
 let div = document.createElement('div');
+let tooltip = document.createElement('div');
 
 function checkComment() {
     while (commentArea.value === '') {
@@ -13,15 +14,40 @@ function checkComment() {
 
 function validate() {
     if (!regexp.test(phoneArea.value)) {
-        div.className = "invalid";
-        div.innerText = "Invalid number!";
+        div.className = 'invalid';
+        div.innerText = 'Invalid number!';
         td.append(div);
     } else {
-        div.className = "valid";
-        div.innerText = "Valid number!";
+        div.className = 'valid';
+        div.innerText = 'Valid number!';
         td.append(div);
     };
 }
 
+function showTooltip(e) {
+  let tooltipHTML = e.target.dataset.tooltip;
+  tooltip.className = 'tooltip';
+  tooltip.innerHTML = tooltipHTML;
+  document.body.append(tooltip);
+
+  let coords = e.target.getBoundingClientRect();
+  let left = coords.left + (e.target.offsetWidth - tooltip.offsetWidth) / 2;
+  if (left < 0) left = 0; 
+  let top = coords.top - tooltip.offsetHeight - 5;
+  if (top < 0) {
+    top = coords.top + e.target.offsetHeight + 5;
+  }
+  tooltip.style.left = left + 'px';
+  tooltip.style.top = top + 'px';
+}
+
+function hideTooltip(e) {
+  if (tooltip) {
+    tooltip.remove();
+  }
+};
+
 btn.addEventListener('click', checkComment);
 btn.addEventListener('click', validate);
+commentArea.addEventListener('mouseover', showTooltip);
+commentArea.addEventListener('mouseleave', hideTooltip);
